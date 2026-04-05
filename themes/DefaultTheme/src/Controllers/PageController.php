33747ec28f0e9cc4b5ab21267be5ec38cc6f8d7a
Abolfazlyousefii <150295740+Abolfazlyousefii@users.noreply.github.com>
2026-04-05 10:22:58 +0330

    Introduce /blog routes and map blog CMS page to blog index

--- themes/DefaultTheme/src/Controllers/PageController.php
+++ themes/DefaultTheme/src/Controllers/PageController.php
@@ -8,10 +8,22 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function show($page)
+    public function show(Request $request, $page)
     {
         $page = Page::detectLang()->where('slug', $page)->orWhere('id', $page)->firstOrFail();
 
+        if ($this->isBlogPage($page)) {
+            return redirect()->route('front.blog.index', $request->query());
+        }
+
         return view('front::pages.show', compact('page'));
+    }
+
+    private function isBlogPage(Page $page): bool
+    {
+        $slug = mb_strtolower(trim($page->slug));
+        $title = mb_strtolower(trim($page->title));
+
+        return in_array($slug, ['blog', 'وبلاگ'], true) || in_array($title, ['blog', 'وبلاگ'], true);
     }
 }
